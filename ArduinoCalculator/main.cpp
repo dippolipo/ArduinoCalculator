d6183aa0c341d8df308f3b192685fdf3fce7d036
dippolipo <159489652+dippolipo@users.noreply.github.com>
2024-10-19 14:02:16 +0200

    aggiunta gestione degli errori

--- ArduinoCalculator/main.cpp
+++ ArduinoCalculator/main.cpp
@@ -46,10 +46,14 @@
 
 #define maxInputLength 50
 
+#define UERROR 1
+#define OERROR 2
+
 double ans;
 double a;
 double b;
 double c;
+double numbers[maxInputLength / 2 + 1];
 byte stringCursorPos;
 byte inputStartingPos;
 byte stringStartingPos;
@@ -59,27 +63,27 @@ short int input[maxInputLength];
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 byte inputs[maxInputLength];
-byte inputs1[maxInputLength];
-byte inputs2[maxInputLength];
-byte inputs3[maxInputLength];
 byte pars[maxInputLength / 2 + 1][3];
+byte operators[maxInputLength / 2];
+byte error = 0;
 
+byte checkInputs();
 void getInputsCalc();
 void printInputs(byte *inArray);
 void newCalc();
 void clearSolving();
 void printCalc(byte stringShift);
 byte printCursor(short int movement);
+void removeData(short int& firstNum, short int delta, short int& currentPar, short int& firstParAnalyzed);
+double solve();
+void printSol();
+void inBetween();
 
 void setup() {
   ans = 0;
   a = 0;
   b = 0;
   c = 0;
-  for (int i = 0; i < maxInputLength && inputs[i] != 255; i++) {
-    inputs[i] = 255;
-  }
-  newCalc();
 
   Serial.begin(9600); //DEBUG
 
@@ -95,59 +99,37 @@ void setup() {
   lcd.print("---D'Ippolito---");
   lcd.setCursor(0,1);
   lcd.print("premere un tasto");
-  getInputsCalc();
 }
 
 void loop() {
-  byte bit0 = digitalRead(BIT0PIN);
-  byte bit1 = digitalRead(BIT1PIN);
-  byte bit2 = digitalRead(BIT2PIN);
-  byte bit3 = digitalRead(BIT3PIN);
-  byte bit4 = digitalRead(BIT4PIN);
-  int buttonID = bit0 + bit1 * 2 + bit2 * 4 + bit3 * 8 + bit4 * 16;
-
-  short int inputChars; // numero di caratteri corrispondenti
-  
-  Serial.println(buttonID);
-
-  lcd.setCursor(0, 0);
-  if (buttonID == 0) {
-    Serial.println("no signal");
-    lcd.print("no signal");
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-  } else {
-    lcd.clear();
-    lcd.print(bit4);
-    lcd.setCursor(1, 0);
-    lcd.print(bit3);
-    lcd.setCursor(2, 0);
-    lcd.print(bit2);
-    lcd.setCursor(3, 0);
-    lcd.print(bit1);
-    lcd.setCursor(4, 0);
-    lcd.print(bit0);
-    lcd.noAutoscroll();
-    lcd.setCursor(0, 1);
-    lcd.print(buttonID); 
-  }
-  
-  Serial.println(" ");
-
-  //delay(1000);
+  newCalc();
+  getInputsCalc();
+  fromInputToEquation();
+  ans = solve();
+  printSol();
+  delay(2000);
+  inBetween();
 }
 
-void getInputsCalc() {
-  byte input = 255;
+byte checkInputs(byte lastInput) {
+  delay(20);
+
+  byte input = 0;
   bool shift = false;
   byte buttonDead = 0; // 0 = bottone è premuto
-  byte lastInput;
+
+  while (lastInput != 0) {
+    lastInput = 0;
+    lastInput += digitalRead(BIT0PIN);
+    lastInput += digitalRead(BIT1PIN);
+    lastInput += digitalRead(BIT2PIN);
+    lastInput += digitalRead(BIT3PIN);
+    lastInput += digitalRead(BIT4PIN);
+  }
+
   byte check = 0;
 
-  short int movement;
-  byte cursor = 0;
-
-  do {
+  while (check < 8) {
     input = 0;
     input += digitalRead(BIT0PIN);
     input += digitalRead(BIT1PIN) * 2;
@@ -157,44 +139,36 @@ void getInputsCalc() {
 
     if (input != 0) {
       input = input | lastInput;
+      lastInput = input;
       check++;
-      
-      if (check < 5) {
-        continue;
-      } else {
-        check = 128;
-      }
-
-      if (buttonDead < 10) {
-        continue;
-      }
-      else {
-        buttonDead = 0;
-      }
     } else {
       check = 0;
-      buttonDead += (buttonDead < 255) ? 1 : 0;
       lastInput = 0;
-      continue;
     }
+  }
+  return input;
+}
 
-    Serial.print("input: ");
-    Serial.println(input);
+void getInputsCalc() {
+  byte input = 0;
+  bool shift = false;
+  short int movement;
+  byte cursor = 0;
 
-    if (input == 30) {        // shift
+  do {
+    input = checkInputs(input);
+
+    if (input == 30) { // shift
       movement = 0;
       shift = true;
       continue;
-    } else if (input <= _ans_) {
-      Serial.println("CHAR!");
+    } else if (input <= 26) { // è un qualcosa da inserire nel calcolo
       if (inputs[cursor] != 255 && inputs[maxInputLength - 1] == 255) {
         for (int i = maxInputLength - 1; i >= cursor; i--) {
           inputs[i] = inputs[i-1];
         }
       }
       inputs[cursor++] = input;
-      Serial.print("input: ");
-      Serial.println(inputs[cursor - 1]);
       if (input > _pow_ && shift) {
         inputs[cursor - 1] += 8;
       }
@@ -229,7 +203,6 @@ void getInputsCalc() {
       }
     } else if (input == 28) { // ->
       if (inputs[cursor] != 255 && cursor + 1 < maxInputLength) {
-        Serial.println("Right movement");
         if ((inputs[cursor] > _ln_ && inputs[cursor] <= _hta_) || (inputs[cursor] >= _sin_ && inputs[cursor] < _ln_)) {
           movement = 3;
         } else if (inputs[cursor] == _ln_) {
@@ -249,7 +222,6 @@ void getInputsCalc() {
         } else {
           movement = -1;
         }
-        Serial.println("Left movement");
       } /*else { // sposta il cursore alla fine del calcolo
         movement = 3; //TODO
         for (int i = 0; inputs[i] != 255 && i < maxInputLength; i--) {
@@ -257,17 +229,12 @@ void getInputsCalc() {
         }
       }*/
     } else if (input == 31) { // =
-      //fromInputToEquation();TODO
-      movement = 4;
+      movement = 255;
       cursor = 0;
-      break;
     }
     
     shift=false;
-    Serial.print("cursor: ");
-    Serial.println(cursor);
-    Serial.print("movement: ");
-    Serial.println(movement);
+
     printCalc(printCursor(movement));
     movement = 0;
     delay(10);
@@ -282,45 +249,24 @@ byte printCursor(short int movement) {
   if (movement == 255) {
     stringShift = 0;
     cursorString = 0;
-    Serial.print("inputs[0]: ");
-    Serial.println(inputs[0]);
-    if ((inputs[0] > _ln_ && inputs[0] <= _hta_) || (inputs[0] >= _sin_ && inputs[0] < _ln_)) {
-      movement = 3;
-    } else if (inputs[0] == _ln_) {
-      movement = 2;
-    } else if (inputs[0] != 255){
-      movement = 1;
-    } else {
-      movement = 0;
-    }
+    movement = 0;
   }
 
-  Serial.print("movement: ");
-  Serial.println(movement);
   cursorString += movement;
 
   if (cursorString >= 16) {
     byte delta = cursorString - 16;
-    Serial.print("delta: ");
-    Serial.println(delta);
-    Serial.print("cursorString: ");
-    Serial.println(cursorString);
     cursorString = 15;
     stringShift += delta + 1;
   } else if (cursorString < 0) {
-    Serial.print("cursorString: ");
-    Serial.println(cursorString);
     stringShift += cursorString;
     cursorString = 0;
   }
   
 
-  Serial.print("cursorString: ");
-  Serial.println(cursorString);
   cursorPrint[cursorString] = 'L';
   lcd.setCursor(0, 1);
   lcd.print(cursorPrint);
-  Serial.println("endPrint");
 
   return stringShift;
 }
@@ -445,20 +391,23 @@ void printCalc(byte shift) {
 
   lcd.setCursor(0, 0);
   lcd.print(toPrint);
-
-  Serial.println(complete);
-  Serial.println(toPrint);
-  Serial.println("endPrint");
 }
 
 void newCalc() {
+  error = 0;
+  for (int i = 0; i < maxInputLength / 2 + 1; i++) {
+    numbers[i] = 0;
+  }
+  pars[0][0] = 0;
+  pars[0][2] = 0;
+  for (int i = 1; i < maxInputLength / 2 + 1; i++) {
+    pars[i][0] = maxInputLength;
+    pars[i][1] = 255;
+    pars[0][2] = 0;
+  }
 	for (int i = 0; i < maxInputLength; i++) {
-    inputs3[i] = inputs2[i];
-    inputs2[i] = inputs1[i];
-    inputs1[i] = inputs[i];
 		inputs[i] = 255;
 	}
-  programState = 1;
 }
 
 void clearSolving() {
@@ -472,4 +421,304 @@ void clearSolving() {
 		pars[i][1] = 255;
 		pars[0][2] = 0;
 	}
+}
+
+short int fromInputToEquation() {
+	byte numNum   = 0; // numero di numeri
+	byte opNum    = 0; // numero di operatori
+	byte parNum   = 0;
+	byte funcNum  = 0;
+
+	bool isPositive = true;
+	short int digitOverZero = 0; // 1 = true, > -1 = false, 0 = sconosciuto
+	byte lastParToClose = 0;
+
+	for (int i = 0; i < maxInputLength; i++) {
+		if (inputs[i] <= _dot_) { // e' una cifra o un punto
+			if (i > 0) {
+				if (inputs[i - 1] == _cpa_) {
+					return 1;
+				}
+			}
+
+
+			if (inputs[i] == _dot_) {
+				if (digitOverZero >= 0) {
+					digitOverZero = -1;
+				}
+				else {
+					return 1;
+				}
+			}
+			else {
+				if (digitOverZero >= 0) {
+          Serial.print("->");
+          Serial.print(numbers[numNum]);
+          Serial.print("*10+");
+          Serial.println(inputs[i]-1);
+					numbers[numNum] = numbers[numNum] * 10 + (inputs[i] - 1);
+					digitOverZero = 1;
+				}
+				else {
+					numbers[numNum] += (inputs[i] - 1) * pow(10, digitOverZero--);
+				}
+			}
+      Serial.print(": ");
+      Serial.println(numbers[0]);
+		}
+		else if (digitOverZero != 0 && (inputs[i] <= _div_ || inputs[i] == _pow_)) {
+			digitOverZero = 0;
+			operators[opNum++] = inputs[i];
+			numbers[numNum++] *= 1 + 2 * (-1 + isPositive); // se il numero e' negativo allora sara' moltiplicato per -1	
+			isPositive = true;
+		}
+		else if (inputs[i] == _min_) {
+			isPositive = !isPositive;
+		}
+		else if (inputs[i] == _plu_) {
+			continue;
+		}
+		else if (inputs[i] == _opa_ || (inputs[i] >= _sqr_ && inputs[i] <= _abs_) || (inputs[i] >= _hsi_ && inputs[i] <= _hta_)) {
+
+			if (digitOverZero != 0) {
+				digitOverZero = 0;
+				operators[opNum++] = _for_;
+				numbers[numNum++] *= 1 + 2 * (-1 + isPositive); // se il numero e' negativo allora sara' moltiplicato per -1
+				isPositive = true;
+			}
+
+			pars[++parNum][2] = (1 - isPositive) * 0x80;
+			pars[parNum][0] = numNum;
+			
+			if (lastParToClose == 0) {
+				lastParToClose = parNum;
+			}
+			isPositive = true;
+
+			if (inputs[i] != _opa_) {
+				pars[parNum][2] = pars[parNum][2] | inputs[i];
+			}
+
+		}
+		else if (inputs[i] == _cpa_ || inputs[i] == _xsq_) {	
+			if (digitOverZero != 0) {
+				for (int j = parNum; j >= lastParToClose; j--) {
+					if (pars[j][1] == 255) {
+						pars[j][1] = numNum;
+						if (j == lastParToClose) {
+							lastParToClose = 0;
+						}
+						if (pars[j][0] == 255) {
+							pars[j][0] = numNum;
+						}
+						break;
+					}
+				}
+			}
+			else {
+				return 1;
+			}
+
+			if (inputs[i] == _xsq_) {
+				operators[opNum++] = inputs[i];
+				numbers[numNum++] *= 1 + 2 * (-1 + isPositive); // se il numero e' negativo allora sara' moltiplicato per -1
+				isPositive = true;
+				
+				pars[++parNum][2] = (1 - isPositive) * 0x80;
+				pars[parNum][0] = numNum;
+				if (lastParToClose == 0) {
+					lastParToClose = parNum;
+				}
+			}
+		}
+		else if (inputs[i] == 255) {
+			numbers[numNum] *= 1 + 2 * (-1 + isPositive);
+			pars[0][1] = numNum;
+			for (int j = parNum; j >= lastParToClose; j--) {
+				if (pars[j][1] == 255) {
+					pars[j][1] = numNum;
+					if (j == lastParToClose) {
+						lastParToClose = 0;
+					}
+					break;
+				}
+			}
+			return 0;
+		}
+		else if (inputs[i] >= _pi_) {
+			if (digitOverZero != 0) {
+				operators[opNum++] = _for_;
+				numbers[numNum++] *= 1 + 2 * (-1 + isPositive); // se il numero e' negativo allora sara' moltiplicato per -1
+				isPositive = true;
+			}
+			switch (inputs[i]) {
+			case _pi_:
+				numbers[numNum] = PI;
+				break;
+			default:
+				return 2;
+				break;
+			}
+			if (inputs[i + 1] <= _dot_) {
+				return 2;
+			}
+			digitOverZero = 1;
+		}
+		else if (inputs[i] != 255) {
+      return 2;
+		}
+	}
+}
+
+void printSol() {
+  lcd.clear();
+  printCalc(0);
+  lcd.setCursor(0,1);
+  switch (error) {
+    case 0:
+      lcd.print(String(ans));
+      break;
+    case UERROR:
+      lcd.print("User Error");
+      break;
+    case OERROR:
+      lcd.print("Overflow Error");
+      break;
+  }
+}
+
+double solve() {
+
+	static byte parAnalyzed = 0;
+	byte currentPar = parAnalyzed;
+
+	while (pars[parAnalyzed + 1][1] != 255 && pars[currentPar][1] >= pars[parAnalyzed + 1][0]) {
+		parAnalyzed++;
+		byte firstParAnalyzed = parAnalyzed;
+		numbers[pars[firstParAnalyzed][0]] = solve();
+		removeData(pars[firstParAnalyzed][0], pars[firstParAnalyzed][1] - pars[firstParAnalyzed][0], currentPar, firstParAnalyzed);
+	}
+
+  if (error != 0) {
+    return 1;
+  }
+
+  double sol = 0;
+
+	for (short int i = pars[currentPar][0]; i < pars[currentPar][1]; i++) { // ^ & xsqr
+    if (operators[i] == _pow_) {
+      numbers[i] = pow(numbers[i], numbers[i + 1]);
+      removeData(i, 1, currentPar, currentPar);
+    } else if (operators[i] == _xsq_) {
+      if (numbers[i+1] < 0) {
+        return 0;
+        error = UERROR;
+        return UERROR;
+      }
+      numbers[i] = pow(numbers[i+1], 1.f / numbers[i]);
+      removeData(i, 1, currentPar, currentPar);
+    }
+	}
+
+	short int firstNOfForDiv = 255;
+	for (int i = pars[currentPar][0]; i < pars[currentPar][1]; i++) { // * & /
+		if (operators[i] > _min_) {
+			firstNOfForDiv = (firstNOfForDiv == 255) ? i : firstNOfForDiv;
+			numbers[firstNOfForDiv] *= (operators[i] == _for_) ? numbers[i + 1] : (1 / numbers[i + 1]);
+			numbers[i + 1] = 0;
+			operators[i] = _plu_;
+			continue;
+		}
+		firstNOfForDiv = 255;
+	}
+
+	sol += numbers[pars[currentPar][0]];
+	for (int i = pars[currentPar][0] + 1; i <= pars[currentPar][1]; i++) { // + & -
+		sol += (operators[i - 1] == _plu_) ? numbers[i] : -numbers[i];
+	}
+
+	bool isPositive = (pars[currentPar][2] > 0x7F);
+	pars[currentPar][2] = pars[currentPar][2] & 0x7F;
+	
+	switch (pars[currentPar][2]) {
+	case _sqr_:
+		if (sol < 0) {
+			error = UERROR;
+      return UERROR;
+		}
+		sol = sqrt(sol);
+		break;
+	case _sin_:
+		sol = sin(sol);
+		break;
+	case _cos_:
+		sol = cos(sol);
+		break;
+	case _tan_:
+		sol = tan(sol);
+		break;
+	case _log_:
+		if (sol <= 0) {
+			error = UERROR;
+      return UERROR;
+		}
+		sol = tan(sol);
+		break;
+	case _ln_:
+		if (sol <= 0) {
+			error = UERROR;
+      return UERROR;
+		}
+		sol = tan(sol);
+		break;
+	case _abs_:
+		sol = abs(sol);
+		break;
+	case _hsi_:
+		if (sol < -1 || sol > 1) {
+			error = UERROR;
+      return UERROR;
+		}
+		sol = asin(sol);
+		break;
+	case _hco_:
+		if (sol < -1 || sol > 1) {
+			error = UERROR;
+      return UERROR;
+		}
+		sol = acos(sol);
+		break;
+	case _hta_:
+		sol = atan(sol);
+		break;
+	}
+	return sol * (1 - 2 * isPositive);
+}
+
+void removeData(byte firstNum, short int delta, byte currentPar, byte firstParAnalyzed) {
+
+	for (int i = firstNum + 1; i <= pars[0][1] - delta; i++) {
+		numbers[i] = numbers[i + delta];
+		operators[i - 1] = operators[i + delta - 1];
+	}
+
+	for (int i = 1; i < firstParAnalyzed; i++) {
+		if (pars[i][1] >= pars[currentPar][1]) {
+			pars[i][1] -= delta;
+		}
+	}
+
+	for (int i = firstParAnalyzed + 1; pars[i][0] != maxInputLength; i++) {
+		pars[i][0] -= delta;
+		pars[i][1] -= delta;
+	}
+
+	pars[0][1] -= delta;
+}
+
+void inBetween() {
+  byte input = 1;
+  while (input == 0) {
+    input = checkInputs(input);
+  }
 }
